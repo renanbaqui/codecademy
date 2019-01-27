57c4e2da3ec24db2a2db0c0dd14bdcdf5d2799e6
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2019-01-27 01:40:07 -0200

    Create CarLoan.java

---
+++ CarLoan.java
@@ -0,0 +1,24 @@
+// A program that calculates a car loan payment
+public class CarLoan {
+	public static void main(String[] args) {
+		int carLoan = 10000;
+    int loanLenght = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+    
+    if(loanLenght <= 0 || interestRate <= 0) {
+    	System.out.println("Error! You must take out a valid car loan.");
+    }
+    else if(downPayment >= carLoan) {
+      System.out.println("The car can be paid in full.");
+    }
+    else {
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLenght * 12;
+      int monthlyBalance = remainingBalance / months;
+      int interest = (monthlyBalance * interestRate) / 100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println(monthlyPayment);
+    }
+  }
+}
